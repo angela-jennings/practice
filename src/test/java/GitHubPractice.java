408b6e66a9897471a0e8e8df9d471d968623e18f
angela-jennings <amjennings118@gmail.com>
2021-07-08 17:50:25 -0600

    commit 1

--- src/test/java/GitHubPractice.java
+++ src/test/java/GitHubPractice.java
@@ -3,5 +3,8 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class GitHubPractice {
     public static void main(String[] args) {
         System.out.println("Hello");
+        //practice
+        //practice
+        //practice
     }
 }
